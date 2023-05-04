fba68cdf1f4258188af38f0db38f5f8acf31fed7
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-05-03 21:19:30 -0500

    update

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,11 +1,7 @@
 import React from 'react';
     
 const PersonCard = (props) => {
-    // We can assume props looks like an object literal with 2 keys and values because of what was passed in
-    // {
-    //    "firstName":"Bill",
-    //    "lastName":"Justice"
-    // }
+    
     const { firstName, lastName, age, hairColor } = props
     return (
         <div>
