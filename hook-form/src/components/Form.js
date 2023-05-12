44a167470ed15173f8462a779b03a08d9051256c
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-05-12 11:38:11 -0500

    updated with validations

--- hook-form/src/components/Form.js
+++ hook-form/src/components/Form.js
@@ -8,29 +8,44 @@ const Form = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    
 
     return (
-        <form>
+        <form onSubmit={() => {}} >
             
             <div>
                 <label htmlFor='firstName'>First Name </label> 
                 <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) } />
+                {
+                    firstName.length < 2 && firstName.length > 0 ? (
+                        <p>First Name must be at least 2 characters</p>
+                      ) : ""
+                }            
             </div>
             <div>
                 <label htmlFor='lastName'>Last Name </label> 
                 <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) } />
+                {
+                    lastName.length < 2 && lastName.length > 0 ? (
+                        <p>Last Name must be at least 2 characters</p>
+                      ) : ""
+                }
             </div>
             <div>
                 <label htmlFor='email'>Email Address </label> 
-                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                <input type="email" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                {email.length < 5 && email.length > 0 ? <p>Email must be at least 5 characters</p>: ""}
             </div>
             <div>
                 <label htmlFor='password'>Password </label>
-                <input type="text" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                {password.length < 8 && password.length > 0 ? (
+                <p>Password must be at least 8 characters</p> ) : ""}
             </div>
             <div>
                 <label htmlFor='confirmPassword'>Confirm Password </label>
-                <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
+                <input type="password" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
+                {confirmPassword !== password ? <p>Passwords must match</p> : ""}
             </div>
             
             <div>
