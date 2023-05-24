e13164b527317018835db192aa9b89958a6c086b
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-05-23 19:07:01 -0500

    updates

--- Faker_API/server/server.js
+++ Faker_API/server/server.js
@@ -1,14 +1,54 @@
 const  express = require('express');
+const {faker} = require('@faker-js/faker');
 const app = express();
 const port = 8000;
 
-app.get("/api", (req, res) => {
-    res.json({ message: "yo World" });
+const createUser = () => ({
+    _id: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    phone: faker.phone.number(),
+    password: faker.internet.password(),
+  } 
+    
+  );
+
+  const createCompany = () => ({
+    _id: faker.string.uuid(),
+    name: faker.company.name(),
+    address: {
+        street: faker.location.streetAddress(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zipcode: faker.location.zipCode(),
+        country: faker.location.country(),
+    },
+  });
+
+app.get("/api/users/new", (req, res) => {
+    const newUser = createUser();
+    res.json(newUser);
+    console.log(newUser);
 });
 
-app.get("/api/dog", (req, res) => {
-    res.json({ message: "my dog" });
+app.get("/api/companies/new", (req, res) => {
+    const newCompany = createCompany();
+    res.json(newCompany);
+    console.log(newCompany);
 });
-  
+
+app.get("/api/user/company", (req, res) => {
+    const newUser = createUser();
+    const newCompany = createCompany();
+    const responseObject = {
+      user: newUser,
+      company: newCompany,
+    };
+    res.json(responseObject);
+    console.log(responseObject);
+  });
+
+
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
