5525dfbfd6d8baacd38e050452d3635c68934c60
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-06-14 10:50:34 -0500

    updates

--- product_manager/client/src/components/Detail.js
+++ product_manager/client/src/components/Detail.js
@@ -16,12 +16,12 @@ const Detail = (props) => {
             .catch( err => console.log(err) );
     }, []);
 
-    const deleteHandler = () => {
+    const deleteHandler = (id) => {
         axios.delete(`http://localhost:8000/api/products/${id}`)
             .then((res) => {
                 console.log(res);
                 console.log(res.data);
-                navigate("/");
+                navigate("/")
             })
             .catch((err) => {
                 console.log(err);
@@ -30,10 +30,11 @@ const Detail = (props) => {
 
     return (
         <div className="detail-component">
+            <h2>Product Detail</h2>
             <p>Title:  {product.title}</p>
             <p>Price:  ${product.price}</p>
             <p>Description:  {product.description}</p>
-            <button onClick={deleteHandler}>Delete</button>
+            <button onClick={() => deleteHandler(product._id)}>Delete Product</button>
         </div>
     );
 }
